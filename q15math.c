5a0d2938647cac38023bb02be04aff2c3d4d3f33
Jason R. Jones <slightlynybbled@gmail.com>
2016-01-24 22:18:47 -0500

    Added basic operations, including absolute value

--- q15math.c
+++ q15math.c
@@ -58,4 +58,49 @@ q15_t q15_from_int(int num){
     return value;
 }
 
+q15_t q15_mul(q15_t multiplicand, q15_t multiplier){
+    uint32_t product = ((uint32_t)multiplicand * (uint32_t)multiplier) >> 16;
+    return (q15_t)product;
+}
 
+q15_t q15_div(q15_t dividend, q15_t divisor){
+    q15_t quotient;
+    
+	/* check to ensure dividend is smaller in magnitude
+     * than the divisor */
+	if(q15_abs(divisor) > q15_abs(dividend)){
+		/* saturation: if signs are different,
+         * then saturate negative */
+		if((divisor & 0x8000) ^ (dividend & 0x8000)){
+			quotient = -32768;
+		}else{
+			quotient = 32767;
+		}
+	}else{
+		/* this is where you should insert the specific
+         * division instruction for this processor – if present */
+		quotient = dividend/divisor;
+	}
+    
+	return quotient;
+}
+
+q15_t q15_add(q15_t addend, q15_t adder){
+    int32_t result = (uint32_t)addend + (uint32_t)adder;
+    
+    if(result > 32767)          result = 32767;
+    else if(result < -32768)    result = -32768;
+    
+    return (q15_t)result;
+}
+
+q15_t q15_abs(q15_t num){
+    q15_t value = num;
+    
+    if(value < 0){
+        if(value < -32767)  value = 32767;
+        else                value = -value;
+    }
+    
+    return value;
+}
