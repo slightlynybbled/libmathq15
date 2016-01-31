c4be90c69257087ac5be15124e64b019dcc5cd71
Jason R. Jones <jjones@updsys.com>
2016-01-30 20:54:53 -0500

    Modified speed file and measurements for Microchip 24-bit instruction devices

--- speed/MicrochipXC16/main.c
+++ speed/MicrochipXC16/main.c
@@ -2,13 +2,13 @@
  * File:   main.c
  * Author: Jason
  *
- * Code used for testing speed of floats vs. fixed.  Used
- * the microchip debugger to test.
+ * Created on January 29, 2016, 8:34 AM
  */
 
 
 #include "xc.h"
 #include "libmathq15.h"
+#include <libq.h>
 
 int main(void) {
     float dNum0, dNum1, dNum;
@@ -18,26 +18,27 @@ int main(void) {
     /* benchmark of floating-point multiplication */
     dNum0 = 0.4286;
     dNum1 = 0.7582;
-
-    dNum = dNum0 * dNum1;
-
-    /* benchmark of fixed-point multiplication */
     fNum0 = (q15_t)(dNum0 * 32767);
     fNum1 = (q15_t)(dNum1 * 32767);
 
+    dNum = dNum0 * dNum1;
+    
+    /* benchmark of fixed-point multiplication - microchip fixed-point lib */
+    /* not provided */
+
+    /* benchmark of fixed-point multiplication - q15 lib*/
     fNum = q15_mul(fNum0, fNum1);
     
-    
-    
-    
+
+
     /**********************************************/
     /* benchmark of floating-point division */
     dNum = dNum0 / dNum1;
+    
+    /* benchmark of fixed-point division - microchip fixed-point lib */
+    /* not provided */
 
-    /* benchmark of fixed-point multiplication */
-    fNum0 = (q15_t)(dNum0 * 32767);
-    fNum1 = (q15_t)(dNum1 * 32767);
-
+    /* benchmark of fixed-point division - libmathq15 */
     fNum = q15_div(fNum0, fNum1);
     
     
@@ -47,14 +48,26 @@ int main(void) {
     /**********************************************/
     /* benchmark of floating-point addition */
     dNum = dNum0 + dNum1;
-
-    /* benchmark of fixed-point multiplication */
-    fNum0 = (q15_t)(dNum0 * 32767);
-    fNum1 = (q15_t)(dNum1 * 32767);
-
-    fNum = q15_add(15000, 1000);
     
+    /* benchmark of fixed-point addition - microchip fixed-point lib */
+    fNum = _Q15add(fNum0, fNum1);
 
+    /* benchmark of fixed-point addition - libmathq15 */
+    fNum = q15_add(fNum0, fNum1);
+    
+    
+    
+    /**********************************************/
+    /* benchmark of floating-point abs - function not provided */
+    /* not provided */
+    
+    /* benchmark of fixed-point abs - microchip fixed-point lib */
+    fNum = _Q15abs(fNum0);
+
+    /* benchmark of fixed-point abs - libmathq15 */
+    fNum = q15_abs(fNum0);
+    
+    Nop();
     
     return 0;
 }
--- speed/readme.md
+++ speed/readme.md
@@ -13,8 +13,11 @@ At this time, I have only run this test on the AVR/arduino.  I am surprised at t
 
 My apologies for mixing standards (micro-seconds above, cycles here), but I know how to use the microchip debugger and I don't know how to use the AVR Studio debugger.  Having said that, I have hand-tuned some of the most common fixed-point operations to work on the 24-bit instruction set from Microchip.  Below are the results.  The methods used for hand-tuning could have taken advantage of the DSP or made other assumptions about the calling functions in order to make the code somewhat more efficient, but the code - as written - does certain bounds checking and saturation which do require some cycles to perform.
 
-|                | float      | q15_t     |
-|----------------|------------|-----------|
-| Multiplication | 156 cycles | 12 cycles |
-| Division       | 494 cycles | 59 cycles |
-| Addition       | 184cycles  | 13 cycles |
+I have also taken a moment to compare - where possible - the equivalent function in Microchip's internal library (provided in 'libq.h' and 'libq-elf.a').  The microchip-provided functions are slightly more efficient, but the source is not directly available.  One could look at the disassembly listing and go through lots of effort to squeeze the last cycle out of it, but I suspect that our efforts are better spent elsewhere.
+
+|                | float        | Microchip fixed-point lib | q15_t     |
+|----------------|--------------|---------------------------|-----------|
+| Multiplication | 156 cycles   | not provided              | 12 cycles |
+| Division       | 494 cycles   | not provided              | 59 cycles |
+| Addition       | 184 cycles   | 12 cycles                 | 13 cycles |
+| Absolute Value | not provided | 8 cycles                  | 11 cycles |
